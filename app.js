ed5a36dc66a134f1cfe4ae99c4fef6be7aaf4708
Amaansyed309 <64450603+Amaansyed309@users.noreply.github.com>
2022-10-05 00:59:33 +0530

    update

--- app.js
+++ app.js
@@ -1,30 +1,20 @@
-const taskItems = document.querySelector("#task-list");
-const taskList = document.querySelector("#task-items");
-const addTaskBtn = document.querySelector("#add-task-btn");
+// const taskItems = document.querySelector(".task-list");
+// const taskList = document.querySelector("#task-items");
+// const addTaskBtn = document.querySelector("#add-task-btn");
 
-function addTaskToList(){
-    let list = document.createElement("li")
-    list.innerHTML = taskItems.value;
-    taskList.appendChild(list)
+// function addTaskToList(){
+//     let list = document.createElement("li")
+//     list.innerHTML = taskItems.value;
+//     taskList.appendChild(list)
     
-}
+// }
 
-addTaskBtn.addEventListener("click", addTaskToList)
+// addTaskBtn.addEventListener("click",addTaskToList)
 
-taskItems.addEventListener("keydown", function(event) {
-    if (!event) {
-       var event = window.event;
-    }
-       event.preventDefault();
-    if (event.keyCode == 13){
-       login();
-    }
- }, false);
- function login(){
-    console.log("submitted successfully....");
- }
+// taskItems.addEventListener("keyup", function(event) {
+//    if (event.keyCode === 13) {
+//        event.preventDefault();
+//        document.getElementById("add-task-btn").click();
+//    }
+// });  
 
-
-function clearTextBox (){
-    taskItems.value = "";
-}
