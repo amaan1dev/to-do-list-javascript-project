d13279283f1ff524f71da875b40f30858fe11fd1
Amaansyed309 <amaan.lko32@gmail.com>
2022-10-05 22:13:03 +0530

    update

--- app.js
+++ app.js
@@ -16,18 +16,27 @@ function addTodo(event) {
 
    // create li 
    const newTodo = document.createElement("li");
-   newTodo.innerText = "hey";
+
+   // appeding value of entered task into the newTodo li
+   newTodo.innerText = todoInput.value;
    newTodo.classList.add("todo-item");
    todoDiv.appendChild(newTodo);
 
    // check mark button
 
    const completedBtn = document.createElement("button");
-   completedBtn.innerHTML = '<i class = "fas fa-check"></i>';
+   completedBtn.innerHTML = '<i class = "fas fa-check"></i>submit';
    completedBtn.classList.add("complete-btn");
    todoDiv.appendChild(completedBtn);
 
    // check trash button
+
+   // insert an emoji inside the todoDiv
+
+   const emojiIcon = document.createElement("div")
+   emojiIcon.innerText = "😘";
+   emojiIcon.classList.add("emoji-icon");
+   todoDiv.appendChild(emojiIcon);
 
    const trashBtn = document.createElement("button");
    trashBtn.innerHTML = '<i class = "fas fa-trash"></i>';
